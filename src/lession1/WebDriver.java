041e5e86543785db3b97a972af07fcabdd9ec3e0
tuyennt <tuyennt@tuyennt.officevnpay.com>
2019-11-12 17:25:52 +0700

    CREATE REPO

---
+++ src/lession1/WebDriver.java
@@ -0,0 +1,32 @@
+package lession1;
+
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WebDriver {
+	// khai bao
+	FirefoxDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver","E:\\Congviec\\autotest\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://github.com/mozilla/geckodriver/releases");
+	}
+
+	@Test
+	public void tC01_Login () {
+		System.out.println("TC 01");
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
