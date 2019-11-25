6b436f1be0f04956a96bf4e68b16518a9702efbe
tuyennt <tuyennt@tuyennt.officevnpay.com>
2019-11-25 18:45:08 +0700

    Update lesson 7

--- src/lesson7/PopUp_Windows.java
+++ src/lesson7/PopUp_Windows.java
@@ -2,10 +2,8 @@ package lesson7;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
-
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,73 +13,74 @@ import  org.testng.Assert;
 
 public class PopUp_Windows {
 	WebDriver driver;
-
+	
+	
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver",
 				"E:\\Congviec\\autotest\\geckodriver-v0.26.0-win64\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
 
 	@Test
-	public void TC02_WindowsTab() {
+	public void TC02_WindowsTab() throws Exception {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		String parentWin = driver.getWindowHandle();
 		
 		WebElement google = driver.findElement(By.xpath("//a[text()='GOOGLE']"));
 		google.click();
-		switchToChildWindowByid(parentWin);
-//		switchToChildWindowByTitle("Google");
-//		Assert.assertEquals(driver.getTitle(), "Google");
-//		driver.switchTo().window(parentWin);
-//		
-//		WebElement fb = driver.findElement(By.xpath("//a[text()='FACEBOOK']"));
-//		fb.click();
-////		switchToChildWindowByid(parentWin);
-////		switchToChildWindowByTitle("Facebook - Đăng nhập hoặc đăng ký");
-//		Assert.assertEquals(driver.getTitle(), "Facebook - Đăng nhập hoặc đăng ký");
-//		driver.switchTo().window(parentWin);
-//		
-//		WebElement tiki = driver.findElement(By.xpath("//a[text()='TIKI']"));
-//		tiki.click();
-////		switchToChildWindowByid(parentWin);
-////		switchToChildWindowByTitle("Mua Hàng Trực Tuyến Uy Tín với Giá Rẻ Hơn tại Tiki.vn");
-//		Assert.assertEquals(driver.getTitle(), "Mua Hàng Trực Tuyến Uy Tín với Giá Rẻ Hơn tại Tiki.vn");
-//		
-//		if(closeAllWindows(parentWin)) {
-//			Assert.assertEquals(driver.getTitle(), "SELENIUM WEBDRIVER FORM DEMO");
-//		}else {
-//			System.out.println("Fail");
-//		}
+		Thread.sleep(3000);
+//		switchToChildWindowByid(parentWin);
+		switchToChildWindowByTitle("Google");
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		driver.switchTo().window(parentWin);
+		WebElement fb = driver.findElement(By.xpath("//a[text()='FACEBOOK']"));
+		fb.click();
+		Thread.sleep(3000);
+		switchToChildWindowByTitle("Facebook - Đăng nhập hoặc đăng ký");
+		Assert.assertEquals(driver.getTitle(), "Facebook - Đăng nhập hoặc đăng ký");
+		driver.switchTo().window(parentWin);
+		
+		WebElement tiki = driver.findElement(By.xpath("//a[text()='TIKI']"));
+		tiki.click();
+		Thread.sleep(3000);
+		switchToChildWindowByTitle("Mua Hàng Trực Tuyến Uy Tín với Giá Rẻ Hơn tại Tiki.vn");
+		Assert.assertEquals(driver.getTitle(), "Mua Hàng Trực Tuyến Uy Tín với Giá Rẻ Hơn tại Tiki.vn");
+		
+		if(closeAllWindows(parentWin)) {
+			Assert.assertEquals(driver.getTitle(), "SELENIUM WEBDRIVER FORM DEMO");
+		}else {
+			System.out.println("Fail");
+		}
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-	public void switchToChildWindowByid(String parent) {
+	public void switchToChildWindowByid(String parent) throws Exception{
 		Set<String> allWindows = driver.getWindowHandles();
-		System.out.println(allWindows.size());
 		for (String tab : allWindows) {
-			if (!tab.equals(parent)) {
+			if (!tab.contentEquals(parent)) {
 				driver.switchTo().window(tab);
-				System.out.println(driver.getTitle());
 				break;
 			}
 		}
-		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
 	}
-	public void switchToChildWindowByTitle(String title) {
+	public void switchToChildWindowByTitle(String title) throws Exception {
 		Set<String> allWindows = driver.getWindowHandles();
 		for (String tab : allWindows) {
 			driver.switchTo().window(tab);
+			Thread.sleep(3000);
 			String currentTitle = driver.getTitle();
 			if (currentTitle.equals(title)) {
 				break;
 			}
 		}
-		
 	}
 	public boolean closeAllWindows(String parent) {
 		Set<String> allWindows = driver.getWindowHandles();
